7ff282c872e4ba6f174d23665e959428ea472fa6
FreddyCro <freddy10100@hotmail.com>
2022-08-17 08:04:34 +0800

    feat: dev

--- src/subpages/fisher/main.js
+++ src/subpages/fisher/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import index from './index.vue'
+import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(index),
+  render: h => h(App),
 }).$mount('#app')
