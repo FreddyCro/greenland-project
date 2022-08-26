7d65062072290a6aa817d425df9fe88377b15cc9
FreddyCro <freddy10100@hotmail.com>
2022-08-26 18:04:16 +0800

    feat: pic component

--- src/components/g-pic.vue
+++ src/components/g-pic.vue
@@ -1,7 +1,6 @@
 <template>
   <picture class="g-vid">
     <template v-for="(media, index) in parsedMedia">
-      {# webp #}
       <source
         v-if="webp"
         :srcset="parsedWebpSrcset[index]"
@@ -9,8 +8,6 @@
         type="image/webp"
         :key="`webp-${media}`"
       />
-
-      {# jpg/png #}
       <source
         v-if="srcset.length > 0"
         :srcset="parsedSrcset[index]"
@@ -18,7 +15,7 @@
         :key="`normal-${media}`"
       />
     </template>
-    <img src="{{ src }}" alt="{{ alt }}" :loading="loading" />
+    <img :src="`${src}_pc.${ext}`" :alt="alt" :loading="loading" />
   </picture>
 </template>
 
@@ -64,32 +61,79 @@ export default {
       default: false,
     },
   },
-  compouted: {
+  computed: {
     parsedMedia() {
       if (!this.srcset) return;
       if (this.srcset.length === 0) return;
 
-      return this.srcset.map((item) => {
-        return `(min-width: ${item.size}px)`;
-      });
+      const media = [];
+
+      if (this.srcset.includes('mob')) {
+        media.push('(min-width: 768px)');
+      }
+
+      if (this.srcset.includes('pad')) {
+        media.push('(min-width: 1024px)');
+      }
+
+      return media;
     },
     parsedSrcset() {
       if (!this.srcset) return;
       if (this.srcset.length === 0) return;
 
-      return this.srcset.map((item) => {
-        // TODO: @2x and @3x
-        return `${item.src}.${this.ext}`;
-      });
+      const srcset = [];
 
-      // return this.srcset.map((item) => {
-      //   return `${item.src}-${item.size}.${this.ext} ${item.size}w`;
-      // }).join(', ');
+      if (this.srcset.includes('mob')) {
+        let mob = `${this.src}_mob.${this.ext} 1x`;
+
+        if (this.use2x) {
+          mob += `, ${this.src}_mob@2x.${this.ext} 2x`;
+        }
+
+        if (this.use3x) {
+          mob += `, ${this.src}_mob@3x.${this.ext} 3x`;
+        }
+
+        srcset.push(mob);
+      }
+
+      if (this.srcset.includes('pad')) {
+        let pad = `${this.src}_pad.${this.ext} 1x`;
+
+        if (this.use2x) {
+          pad += `, ${this.src}_pad@2x.${this.ext} 2x`;
+        }
+
+        if (this.use3x) {
+          pad += `, ${this.src}_pad@3x.${this.ext} 3x`;
+        }
+
+        srcset.push(pad);
+      }
+
+      if (this.srcset.includes('pc')) {
+        let pc = `${this.src}_pc.${this.ext} 1x`;
+
+        if (this.use2x) {
+          pc += `, ${this.src}_pc@2x.${this.ext} 2x`;
+        }
+
+        if (this.use3x) {
+          pc += `, ${this.src}_pc@3x.${this.ext} 3x`;
+        }
+
+        srcset.push(pc);
+      }
+
+      return srcset;
     },
     parsedWebpSrcset() {
       if (!this.webp) return;
       if (!this.srcset) return;
       if (this.srcset.length === 0) return;
+
+      return [];
     },
   },
 };
--- src/pages/living/app.vue
+++ src/pages/living/app.vue
@@ -1,19 +1,29 @@
-<template>
-  <div id="app">
-    123
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+#app.greenland
+  hello-world(msg="Welcome to Your Vue.js App")
+  g-pic(
+    src="/img/fakers/faker_ui"
+    :srcset="['mob', 'pad', 'pc']"
+    ext="png"
+    alt="Welcome to Your Vue.js App"
+    :webp="true"
+  )
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue';
+
+import GPic from '@/components/g-pic.vue';
+import GVid from '@/components/g-vid.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    GPic,
+    GVid,
+  },
+};
 </script>
 
 <style>
