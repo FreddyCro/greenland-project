ba212aa8b592436a04c18d429608c397e2782d15
FreddyCro <freddy10100@hotmail.com>
2022-09-01 08:17:43 +0800

    feat: meta and content

--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,32 @@
 const landingString = require('./src/assets/string/landing.json');
-// const climateString = require('./src/assets/string/climate.json');
-// const farmingString = require('./src/assets/string/farming.json');
-// const fishingString = require('./src/assets/string/fishing.json');
-// const livingString = require('./src/assets/string/living.json');
+const climateString = require('./src/assets/string/climate.json');
+const farmingString = require('./src/assets/string/farming.json');
+const fishingString = require('./src/assets/string/fishing.json');
+const livingString = require('./src/assets/string/living.json');
+
 const path = require('path');
 const resolve = (dir) => path.join(__dirname, dir);
 
 const ENV = 'test';
 
-/* production */
-const productionPath =
-  process.env.NODE_ENV === 'production'
-    ? 'https://vip.udn.com/newmedia/2022/greenland/'
-    : 'http://localhost:8080/';
+const getPublicPath = (env) => {
+  /* production */
+  const productionPath =
+    process.env.NODE_ENV === 'production'
+      ? 'https://vip.udn.com/newmedia/2022/greenland/'
+      : 'http://localhost:8080/';
 
-/* testing */
-const testingPath =
-  process.env.NODE_ENV === 'production'
-    ? 'https://freddycro.github.io/test-page/'
-    : 'http://localhost:8080/';
+  /* testing */
+  const testingPath =
+    process.env.NODE_ENV === 'production'
+      ? 'https://freddycro.github.io/test-page/'
+      : 'http://localhost:8080/';
 
-const publicPath = ENV === 'production' ? productionPath : testingPath;
+  return env === 'production' ? productionPath : testingPath;
+};
+
+const publicPath = getPublicPath(ENV);
+const robot = ENV === 'production' ? 'index,follow' : 'noindex,nofollow';
 
 module.exports = {
   publicPath,
@@ -32,22 +38,73 @@ module.exports = {
       metaTitle: landingString.metaTitle,
       metaDescription: landingString.metaDescription,
       metaUrl: landingString.metaUrl,
+      metaImage: `${landingString.metaUrl}/og.jpg`,
+      metaKeywords: landingString.metaKeywords,
+      metaRobot: robot,
+      metaAuthor: landingString.metaAuthor,
+      metaCopyright: landingString.metaCopyright,
+      metaPublish: landingString.metaPublish,
+      metaModified: landingString.metaModified,
     },
     climate: {
       entry: 'src/pages/climate/main.js',
       filename: 'climate/index.html',
+      title: climateString.metaTitle,
+      metaTitle: climateString.metaTitle,
+      metaDescription: climateString.metaDescription,
+      metaUrl: climateString.metaUrl,
+      metaImage: `${climateString.metaUrl}/og.jpg`,
+      metaKeywords: climateString.metaKeywords,
+      metaRobot: robot,
+      metaAuthor: climateString.metaAuthor,
+      metaCopyright: climateString.metaCopyright,
+      metaPublish: climateString.metaPublish,
+      metaModified: climateString.metaModified,
     },
     farming: {
       entry: 'src/pages/farming/main.js',
       filename: 'farming/index.html',
+      title: farmingString.metaTitle,
+      metaTitle: farmingString.metaTitle,
+      metaDescription: farmingString.metaDescription,
+      metaUrl: farmingString.metaUrl,
+      metaImage: `${farmingString.metaUrl}/og.jpg`,
+      metaKeywords: farmingString.metaKeywords,
+      metaRobot: robot,
+      metaAuthor: farmingString.metaAuthor,
+      metaCopyright: farmingString.metaCopyright,
+      metaPublish: farmingString.metaPublish,
+      metaModified: farmingString.metaModified,
     },
     fishing: {
       entry: 'src/pages/fishing/main.js',
       filename: 'fishing/index.html',
+      title: fishingString.metaTitle,
+      metaTitle: fishingString.metaTitle,
+      metaDescription: fishingString.metaDescription,
+      metaUrl: fishingString.metaUrl,
+      metaImage: `${fishingString.metaUrl}/og.jpg`,
+      metaKeywords: fishingString.metaKeywords,
+      metaRobot: robot,
+      metaAuthor: fishingString.metaAuthor,
+      metaCopyright: fishingString.metaCopyright,
+      metaPublish: fishingString.metaPublish,
+      metaModified: fishingString.metaModified,
     },
     living: {
       entry: 'src/pages/living/main.js',
       filename: 'living/index.html',
+      title: livingString.metaTitle,
+      metaTitle: livingString.metaTitle,
+      metaDescription: livingString.metaDescription,
+      metaUrl: livingString.metaUrl,
+      metaImage: `${livingString.metaUrl}/og.jpg`,
+      metaKeywords: livingString.metaKeywords,
+      metaRobot: robot,
+      metaAuthor: livingString.metaAuthor,
+      metaCopyright: livingString.metaCopyright,
+      metaPublish: livingString.metaPublish,
+      metaModified: livingString.metaModified,
     },
   },
   chainWebpack: (config) => {
